1d574c00953db4972334fcf153e563eacdbc735e
Halit Sever <halitsever@protonmail.com>
2024-04-14 12:47:04 +0300

    fix: 🐛 prevent string in bank module

--- src/modules/bank.module.js
+++ src/modules/bank.module.js
@@ -1,19 +1,17 @@
 const { getUser, setUser } = require("./user.module");
 const strings = require("../lang/strings.tr");
-const database = require("../database/index");
-const userEntity = require("../database/entity/user.entitiy");
 
 const increaseBankAmount = async ({ ctx, increaseAmount }) => {
   if (increaseAmount < 0 || !increaseAmount) return ctx.reply(strings.DATABASE_LOCK);
   const bankInfo = await getUser({ id: ctx?.botInfo.id });
-  bankInfo.balance = bankInfo.balance + increaseAmount;
+  bankInfo.balance = bankInfo.balance + parseInt(increaseAmount);
   await setUser({ user: bankInfo });
 };
 
 const decreaseBankAmount = async ({ ctx, decreaseAmont }) => {
   if (decreaseAmont < 0 || !decreaseAmont) return ctx.reply(strings.DATABASE_LOCK);
   const bankInfo = await getUser({ id: ctx?.botInfo.id });
-  bankInfo.balance = bankInfo.balance - decreaseAmont;
+  bankInfo.balance = bankInfo.balance - parseInt(decreaseAmont);
   await setUser({ user: bankInfo });
 };
 
